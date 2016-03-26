57adc5c6c6b9a1de3a8c34fd2ce279d058690f1e
whitequark <whitequark@whitequark.org>
2016-03-26 12:51:12 +0000

    Expose functions to create individual passes.

--- llvmlite/binding/passmanagers.py
+++ llvmlite/binding/passmanagers.py
@@ -1,5 +1,5 @@
 from __future__ import print_function, absolute_import
-from ctypes import c_bool
+from ctypes import c_bool, c_int
 from . import ffi
 
 
@@ -17,6 +17,54 @@ class PassManager(ffi.ObjectRef):
 
     def _dispose(self):
         self._capi.LLVMPY_DisposePassManager(self)
+
+    def add_constant_merge_pass(self):
+        ffi.lib.LLVMPY_AddConstantMergePass(self)
+
+    def add_dead_arg_elimination_pass(self):
+        ffi.lib.LLVMPY_AddDeadArgEliminationPass(self)
+
+    def add_function_attrs_pass(self):
+        ffi.lib.LLVMPY_AddFunctionAttrsPass(self)
+
+    def add_function_inlining_pass(self, threshold):
+        ffi.lib.LLVMPY_AddFunctionInliningPass(self, threshold)
+
+    def add_global_dce_pass(self):
+        ffi.lib.LLVMPY_AddGlobalDCEPass(self)
+
+    def add_global_optimizer_pass(self):
+        ffi.lib.LLVMPY_AddGlobalOptimizerPass(self)
+
+    def add_ipsccp_pass(self):
+        ffi.lib.LLVMPY_AddIPSCCPPass(self)
+
+    def add_dead_code_elimination_pass(self):
+        ffi.lib.LLVMPY_AddDeadCodeEliminationPass(self)
+
+    def add_cfg_simplification_pass(self):
+        ffi.lib.LLVMPY_AddCFGSimplificationPass(self)
+
+    def add_gvn_pass(self):
+        ffi.lib.LLVMPY_AddGVNPass(self)
+
+    def add_instruction_combining_pass(self):
+        ffi.lib.LLVMPY_AddInstructionCombiningPass(self)
+
+    def add_licm_pass(self):
+        ffi.lib.LLVMPY_AddLICMPass(self)
+
+    def add_sccp_pass(self):
+        ffi.lib.LLVMPY_AddSCCPPass(self)
+
+    def add_sroa_pass(self):
+        ffi.lib.LLVMPY_AddSROAPass(self)
+
+    def add_type_based_alias_analysis_pass(self):
+        ffi.lib.LLVMPY_AddTypeBasedAliasAnalysisPass(self)
+
+    def add_basic_alias_analysis_pass(self):
+        ffi.lib.LLVMPY_AddBasicAliasAnalysisPass(self)
 
 
 class ModulePassManager(PassManager):
@@ -85,3 +133,21 @@ ffi.lib.LLVMPY_FinalizeFunctionPassManager.restype = c_bool
 ffi.lib.LLVMPY_RunFunctionPassManager.argtypes = [ffi.LLVMPassManagerRef,
                                                   ffi.LLVMValueRef]
 ffi.lib.LLVMPY_RunFunctionPassManager.restype = c_bool
+
+ffi.lib.LLVMPY_AddConstantMergePass.argtypes = [ffi.LLVMPassManagerRef]
+ffi.lib.LLVMPY_AddDeadArgEliminationPass.argtypes = [ffi.LLVMPassManagerRef]
+ffi.lib.LLVMPY_AddFunctionAttrsPass.argtypes = [ffi.LLVMPassManagerRef]
+ffi.lib.LLVMPY_AddFunctionInliningPass.argtypes = [ffi.LLVMPassManagerRef, c_int]
+ffi.lib.LLVMPY_AddGlobalDCEPass.argtypes = [ffi.LLVMPassManagerRef]
+ffi.lib.LLVMPY_AddGlobalOptimizerPass.argtypes = [ffi.LLVMPassManagerRef]
+ffi.lib.LLVMPY_AddIPSCCPPass.argtypes = [ffi.LLVMPassManagerRef]
+
+ffi.lib.LLVMPY_AddDeadCodeEliminationPass.argtypes = [ffi.LLVMPassManagerRef]
+ffi.lib.LLVMPY_AddCFGSimplificationPass.argtypes = [ffi.LLVMPassManagerRef]
+ffi.lib.LLVMPY_AddGVNPass.argtypes = [ffi.LLVMPassManagerRef]
+ffi.lib.LLVMPY_AddInstructionCombiningPass.argtypes = [ffi.LLVMPassManagerRef]
+ffi.lib.LLVMPY_AddLICMPass.argtypes = [ffi.LLVMPassManagerRef]
+ffi.lib.LLVMPY_AddSCCPPass.argtypes = [ffi.LLVMPassManagerRef]
+ffi.lib.LLVMPY_AddSROAPass.argtypes = [ffi.LLVMPassManagerRef]
+ffi.lib.LLVMPY_AddTypeBasedAliasAnalysisPass.argtypes = [ffi.LLVMPassManagerRef]
+ffi.lib.LLVMPY_AddBasicAliasAnalysisPass.argtypes = [ffi.LLVMPassManagerRef]
